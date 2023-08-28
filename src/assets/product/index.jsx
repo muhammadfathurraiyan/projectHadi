e601b97e0942b42ccba293a81bfbaea62503ae91
muhammadfathurraiyan <raiyanfathur@gmail.com>
2023-08-28 19:00:59 +0700

    test

--- src/assets/product/index.jsx
+++ src/assets/product/index.jsx
@@ -2,9 +2,19 @@ import React from "react";
 
 const index = () => {
   return (
-    <section className="relative -top-1 bg-green-500 px-24 max-md:px-12 max-sm:px-8 flex items-center justify-center h-screen max-md:h-[120vh]">
-      <div class="absolute bottom-0 left-0 w-full overflow-hidden rotate-180">
-        
+    <section className="relative -top-1 bg-green-500 pt-28 px-24 max-md:px-12 max-sm:px-8 h-screen">
+      <div>
+        <h1 className="uppercase text-xl font-semibold text-white text-center">
+          Lorem ipsum dolor sit amet.
+        </h1>
+        <p className="text-center text-sm leading-4 text-white">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde consequuntur laudantium velit quisquam cupiditate voluptatem.
+        </p>
+      </div>
+      <div className="grid grid-cols-3 gap-4 mt-20">
+        <p>asda</p>
+        <p>asda</p>
+        <p>asda</p>
       </div>
     </section>
   );
