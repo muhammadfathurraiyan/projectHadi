208505a90a08f80907d2ff8614e62495c0aeffb5
muhammadfathurraiyan <raiyanfathur@gmail.com>
2023-08-29 20:01:17 +0700

    update

---
+++ src/assets/product/Card.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { FaCheck } from "react-icons/fa6";
+
+const Card = () => {
+  return (
+    <div className="w-80 bg-white rounded-lg">
+      <img
+        className="h-44 w-80 object-cover rounded-t-lg"
+        src="https://picsum.photos/300"
+        alt=""
+      />
+      <div className="px-6 py-6">
+        <div className="mb-4">
+          <h3 className="font-semibold text-lg">Lorem 1</h3>
+          <p>Rp.12.000</p>
+        </div>
+        <div>
+          <ul>
+            <li className="flex items-center justify-between mb-1">
+              Lorem, ipsum dolor. <span className="text-green-500"><FaCheck /></span>
+            </li>
+            <li className="flex items-center justify-between mb-1">
+              Lorem, ipsum dolor. <span className="text-green-500"><FaCheck /></span>
+            </li>
+            <li className="flex items-center justify-between mb-1">
+              Lorem, ipsum dolor. <span className="text-green-500"><FaCheck /></span>
+            </li>
+          </ul>
+        </div>
+        <a className="bg-green-500 py-3 px-4 inline-block mt-4 rounded-md text-white hover:bg-green-700 duration-300">
+          Button
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/assets/product/index.jsx
+++ src/assets/product/index.jsx
@@ -1,20 +1,35 @@
 import React from "react";
+import Card from "./Card";
 
 const index = () => {
   return (
-    <section className="relative -top-1 bg-green-500 pt-28 px-24 max-md:px-12 max-sm:px-8 h-screen">
+    <section className="relative -top-1 flex flex-col justify-center items-center bg-green-500 py-24 px-24 max-md:px-12 max-sm:px-8">
       <div>
-        <h1 className="uppercase text-xl font-semibold text-white text-center">
+        <h1 className="uppercase text-2xl font-semibold text-white text-center">
           Lorem ipsum dolor sit amet.
         </h1>
         <p className="text-center text-sm leading-4 text-white">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde consequuntur laudantium velit quisquam cupiditate voluptatem.
         </p>
       </div>
-      <div className="grid grid-cols-3 gap-4 mt-20">
-        <p>asda</p>
-        <p>asda</p>
-        <p>asda</p>
+      <div className="mt-14 flex gap-8 max-md:flex-col max-md:items-center">
+        <Card />
+        <Card />
+        <Card />  
+      </div>
+      <div className="absolute left-0 -bottom-[78px] w-full overflow-hidden">
+        <svg
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1200 120"
+          preserveAspectRatio="none"
+          className="relative block h-20 w-[195vw] bg-green-500"
+        >
+          <path
+            d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
+            className="fill-white"
+          ></path>
+        </svg>
       </div>
     </section>
   );
